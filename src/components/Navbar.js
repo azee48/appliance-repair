1abc537aaad9463fc989754e7d92d7ec25a12f06
azee48 <nawazaarib@gmail.com>
2026-04-13 02:36:09 -0400

    ya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,11 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       <div className={`container ${styles.container}`}>
         <Link href="/" className={styles.logo}>
-          <div className={styles.logoIcon}></div>
+          <svg className={styles.logoIcon} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="32" height="32" rx="8" fill="#E85D04"/>
+            <path d="M22.5 9.5C20.6 7.6 17.8 7.1 15.4 8.2L18.5 11.3L17.1 14.6L13.8 16L10.7 12.9C9.6 15.3 10.1 18.1 12 20C13.7 21.7 16.1 22.3 18.3 21.6L23.5 26.8C24.3 27.6 25.6 27.6 26.4 26.8C27.2 26 27.2 24.7 26.4 23.9L21.2 18.7C21.9 16.5 21.3 14.1 22.5 9.5Z" fill="white"/>
+            <circle cx="11" cy="11" r="3" fill="white" fillOpacity="0.3"/>
+          </svg>
           <span className={styles.logoText}>HomeFix Appliance Repair</span>
         </Link>
         <div className={styles.actions}>
